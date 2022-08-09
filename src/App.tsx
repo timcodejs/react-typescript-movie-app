5b28a06d8a6dcb0f50718cd985c630809894d8dd
Tim <46413981+timcodejs@users.noreply.github.com>
2022-08-10 05:03:28 +0900

    파일 정리 및 App.js 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { Wrapper } from './App.style';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { LOAD_ALLPOSTS_REQUEST } from './types/movieTypes';
+import { useDispatch, useSelector } from 'react-redux';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import HomeMain from './pages/homeMain';
+import PopularMain from './pages/popularMain';
+import NowplayingMain from './pages/nowplayingMain';
+import UpcomingMain from './pages/upcomingMain';
+import ToplateMain from './pages/toplateMain';
+import MovieDetail from './components/movieDetail';
+import SearchMain from './pages/searchMain';
 
 function App() {
+  const { loading } = useSelector((state: any) => state.list);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({
+      type: LOAD_ALLPOSTS_REQUEST,
+    });
+  }, []);
+
+  if (loading) return <LinearProgress />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomeMain />}></Route>
+          <Route path='/popular' element={<PopularMain />}></Route>
+          <Route path='/nowplaying' element={<NowplayingMain />}></Route>
+          <Route path='/upcoming' element={<UpcomingMain />}></Route>
+          <Route path='/topLate' element={<ToplateMain />}></Route>
+          <Route path='/detail' element={<MovieDetail />}></Route>
+          <Route path='/search' element={<SearchMain />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Wrapper>
   );
 }
 
