58ead761dde1ee9a528d9f994186a9471d4ae616
Tim <46413981+timcodejs@users.noreply.github.com>
2022-08-10 17:01:23 +0900

    커스텀 훅

---
+++ src/hook/useDetail.ts
@@ -0,0 +1,14 @@
+import { useCallback } from "react";
+import { useNavigate } from 'react-router-dom';
+import { MovieType } from "../types/movieTypes";
+import { SearchType } from "../types/scarchTypes";
+
+export const useDetail = (initialValue: undefined | null = null) => {
+    const navigate = useNavigate();
+
+    const handler = useCallback((e: MovieType | SearchType) => {
+        navigate('/detail', {state: e});
+    }, [navigate]);
+
+    return handler;
+}
